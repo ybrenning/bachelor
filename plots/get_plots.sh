0ab33db4497cf8d9ae32d50d7f7a18fc7cc9f37b
ybrenning <yannickbrenning2@gmail.com>
2024-04-05 17:54:10 +0200

    Update plot stuff

---
+++ plots/get_plots.sh
@@ -0,0 +1,2 @@
+scp -r yb63tadu@ssh.webis.de:/mnt/ceph/storage/data-tmp/current/yb63tadu/active-learning-lab-v2/bert-plots.pdf .
+scp -r yb63tadu@ssh.webis.de:/mnt/ceph/storage/data-tmp/current/yb63tadu/active-learning-lab-v2/setfit-plots.pdf .
